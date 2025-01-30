720dfbd978a8f866ef34952bf694aedffa708213
Aayush Verma <aayush.verma2802@gmail.com>
2025-01-30 20:56:08 +0530

    keras

--- custom_layers.py
+++ custom_layers.py
@@ -1,15 +1,19 @@
-import tensorflow as tf
 import tensorflow_hub as hub
 from tensorflow.keras.layers import Layer
 
 class KerasLayer(Layer):
     def __init__(self, **kwargs):
         super(KerasLayer, self).__init__(**kwargs)
-        self.layer = None
+        self.layer = None  # This will hold the KerasLayer from TFHub
 
     def build(self, input_shape):
-        self.layer = hub.KerasLayer("Plant Disease Detection.h5")  # Update the path accordingly
+        # Here, replace the handle URL with the one you want to use
+        # For example, MobileNetV2's feature vector model
+        self.layer = hub.KerasLayer("https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4")
+        
+        # Now the layer is built with the input shape provided by the model
         self.layer.build(input_shape)
 
     def call(self, inputs):
+        # Forward pass, passing the input to the loaded layer
         return self.layer(inputs)
